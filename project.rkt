67b4006a2cff9cadde1e5fd5dbf9fd3cf52379bd
SaahilClaypool <saahil@claypools.org>
2015-09-27 13:10:22 -0400

    stage

--- project.rkt
+++ project.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname project) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname project) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 ;; Individual Project
 ;; Graphics language to do animation
 
@@ -8,13 +8,13 @@
 ;; - (make-circle 'name radius x y)
 ;; - (make-rectangle 'name width height x y)
 
-;; circle is make circle symbol int int int 
-(define circle (make-circle name radius x y))
+;; circle is (make-circleObject symbol int int int 
+(define-struct circleObject(name radius x y))
 
-;; rectangle is a (make-rectangle symbol int int int int)
-(define rectangle (make-rectangle name width height x y))
+;; rectangle is a (make-rectangleObject symbol int int int int)
+(define-struct rectangleObject (name width height x y))
 
-(define animation (listShape listCmd))
+(define-struct animation (listCmd))
 
 ;; when the running animation, will take list of commands, make list of repeats: Every tick will do all repeats, plus check collision: if coll
 ;; will send event to both objects, let them move, then repaint the canvas. animation will separate list of collision evenents, could 
@@ -27,30 +27,39 @@
 
 ;; addShape consumes shape, adds to 'world'
 ;; (shape -> void)
-(define addShape(shape))
+(define-struct addShape(shape))
 
 ;; deleteShape: symbol -> void
 ;; takes in a shape name, deletes that shape
-(define deleteShape(shape))
+(define-struct deleteShape(shape))
 
 ;; move: symbol int int -> void 
 ;;takes name of shape, moves it by x and y velocity
 
-(define move(shapeName xVel yVel))
+(define-struct move(shapeName xVel yVel))
 
 ;; jump: symbol int int -> void
 ;; takes in a shape name and an x y location
-(define jump(shapeName xLoc yLoc))
+(define-struct jump(shapeName xLoc yLoc))
 
 ;; repeatCommand: (?? -> ??)
 ;; adds a command to be repeated every 'tick' of the world
-(define repeat(command))
+(define-struct repeat(command))
 
-;; addCollisionEvent: symbol comand -> void
+;; addCollisionEvent: object (object -> command) -> void
 ;; takes in a name of object and a comand,
 ;; executes that command if that object is hit
-(define addCollisionEvent (name command))
+(define-struct addCollisionEvent (name command))
+
 
 (define animation1
-  (
-
+  (let ([circle1 (make-circleObject 'circle1 1 1 1)]
+        [rectangle1 (make-rectangleObject 'rectangle1 10 10 10 10)])
+    (make-animation (list
+                     (make-addShape circle1)
+                     (make-addShape rectangle1)
+                     (make-repeat (make-move 'circle1 5 5))
+                     (make-addCollisionEvent circle1
+                                         (lambda (thing-hit)
+                                           (make-deleteShape thing-hit)))))))
+                                         
