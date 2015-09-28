ec253a427ff57e5c347667d940eb4249f577a1a8
SaahilClaypool <saahil@claypools.org>
2015-09-28 14:06:23 -0400

    resolved

--- project.rkt
+++ project.rkt
@@ -1,8 +1,8 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname project) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
-;; Saahil Claypool
+
+;;Saahil Claypool Individual Project
+
 ;; Graphics language to do animation
+(require "world-cs1102.rkt")
 
 
 
@@ -139,6 +139,9 @@
                                              (list (make-jump 'circ1)
                                                    (make-stopAll)))))))
                      
+
+
+
 
 
 
