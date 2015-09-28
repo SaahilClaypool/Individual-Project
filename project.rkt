63d0bb8f57ccb20fa43b0d2169a7b10a17c4497c
SaahilClaypool <saahil@claypools.org>
2015-09-28 13:56:51 -0400

    some touches

--- project.rkt
+++ project.rkt
@@ -1,22 +1,21 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname project) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
-;; Individual Project
+;; Saahil Claypool
 ;; Graphics language to do animation
 
 
-;; TODO: add Stop when ()
+
 ;; addShape ('name shape)
-;;NOTE: might be able to make anon shapes, doesnt really matter, will be searched anyways
 ;; a shape is either
-;; - (make-circle 'name radius x y)
-;; - (make-rectangle 'name width height x y)
+;; - (make-circle 'name radius posn vel)
+;; - (make-rectangle 'name width height posn vel)
 
-;; circle is (make-circleObject symbol int posn  
-(define-struct circleObject (name radius posn))
+;; circle is (make-circleObject symbol int posn vel 
+(define-struct circleObject (name radius posn vel))
 
-;; rectangle is a (make-rectangleObject symbol int int posn )
-(define-struct rectangleObject (name width height posn ))
+;; rectangle is a (make-rectangleObject symbol int int posn vel 
+(define-struct rectangleObject (name width height posn vel))
 ;; an animation is a (make-animation list[cmd]
 ;; takes in a list of commands, creates an animation from this list
 (define-struct animation (listCmd))
@@ -26,8 +25,7 @@
 (define-struct vel(x y))
 
 
-;; when the running animation, will take list of commands, make list of repeats: Every tick will do all repeats, plus check collision: if coll
-;; will send event to both objects, let them move, then repaint the canvas. animation will separate list of collision evenents, could 
+
 
 ;; -------------------------------------------------
 ;; COMMANDS
@@ -36,7 +34,7 @@
 #|a cmd is either a
     (make-addShape symbol shape)
     (make-deleteShape symbol)
-    (make-move symbol vel)
+    (make-setVel symbol vel)
     (make-jump symbol)
     (make-stop symbol)
     (make-horzBounce symbol)
@@ -50,7 +48,7 @@
 |#
 
 ;; addShape (make-addShape  shape), adds to 'world'
-;; (shape -> void)
+
 (define-struct addShape( shape))
 
 ;; deleteShape: (make-deleteShape symbol)
@@ -58,8 +56,8 @@
 (define-struct deleteShape(name))
 
 ;; move: (make-move symbol vel)
-;;takes name of shape, moves it by x and y velocity
-(define-struct move(name vel))
+;;takes name of shape, changes its velocity
+(define-struct setVel(name vel))
 
 
 ;; jump: (make-jump name)
@@ -79,16 +77,14 @@
 
 
 
-;; addCollisionEvent: object object list[cmd]  -> void
+;; addCollisionEvent: (make-addCollisionEvent symbol symbol list[cmd])
 ;; takes in two objects, executes list of events of what should happen when
 ;; the two given objects collide
 ;; either object can be Symbol 'lWall (left wall) 'rWall (right wall) 'tWall (top wall) or 'bWall (bottom wall) this creates event for objects hitting edges
 (define-struct addCollisionEvent (object1 object2  listofcommand))
 
 ;; (make-repeat command)
-;; might not need ````````````````````````````````````````````````````````````````````````````````````````````````` TODO
-;; repeatCommand: (?? -> ??)
-;; adds a command to be repeated every 'tick' of the world
+;; adds a command to a list of commands to be executed every 'tick'
 (define-struct repeat(command))
 
 
@@ -100,40 +96,28 @@
 
 
 
-(define animation1
-  (let ([circle1 (make-circleObject 'circle 1
-                                    (make-posn 1 1))]
-        [rectangle1 (make-rectangleObject 'rectangle1 10 10
-                                          (make-posn 100 10))])
-    (make-animation (list
-                     (make-addShape  circle1)
-                     (make-addShape  rectangle1)
-                     (make-repeat(make-move 'circle1 (make-vel 5 0)))
-                     (make-addCollisionEvent
-                      'circle1
-                      'rectangle1
-                      (list(make-deleteShape 'rectangle1)
-                           (make-horzBounce 'circle1)))))))
 
 (define animationA
   (let ([circle1 (make-circleObject  'rect 10
                                     (make-posn 10 5)
+                                    (make-vel 5 1)
                                    )]
         [rect1 (make-rectangleObject 'circ 5 100
-                                     (make-posn 100 5))])
+                                     (make-posn 100 5)
+                                     (make-vel 0 0 ))])
     (make-animation (list
                      (make-addShape  circle1)
                      (make-addShape  rect1)
                      
-                     (make-repeat (make-move 'circ (make-vel 5 1)))
+                     
                      (make-addCollisionEvent 'circ 'rect1
                                              (list (make-deleteShape 'rect)
                                                    (make-horzBounce 'circ)))
                      (make-addCollisionEvent circle1 'lWall
-                                             (list (make-stop 'circle1)))))))
+                                             (list (make-stopAll)))))))
 
 (define animationB
-  (let ([circ1 (make-circleObject 'circ1 10 (make-posn 100 100) )])
+  (let ([circ1 (make-circleObject 'circ1 10 (make-posn 100 100) (make-vel 0 0))])
     (make-animation (list
                      (make-addShape circ1)
                      (make-repeat (make-jump 'circ1))
@@ -141,21 +125,19 @@
                                              (list (make-stop 'circ1)
                                                    (make-stopAll)))))))
 (define animationC
-  (let ([circ1 (make-circleObject 'circ1 7 (make-posn 20 20) )]
-        [rect1 (make-rectangleObject 'rect  100 10 (make-posn 5 100))])
+  (let ([circ1 (make-circleObject 'circ1 7 (make-posn 20 20) (make-vel 5 5 ))]
+        [rect1 (make-rectangleObject 'rect  100 10 (make-posn 5 100) (make-vel 0 0 ))])
     (make-animation (list
                      (make-addShape circ1)
                      (make-addShape rect1)
-                     (make-repeat (make-move 'circ1 (make-vel 0 5)))
+                     
                      (make-addCollisionEvent 'circ1 'rect1
-                                             (list (make-addShape (make-rectangleObject 'newRect 10 50 (make-posn 80 10) ))
-                                                   (make-vertBounce 'circ1)))
-                     (make-addCollisionEvent 'circ1 'tWall
-                                             (list (make-stop 'circ1)))))))
+                                             (list (make-addShape (make-rectangleObject 'newRect 10 50 (make-posn 80 10) (make-vel 0 0 )))
+                                                   (make-setVel 'circ1 (make-vel 5 0))))
+                     (make-addCollisionEvent 'circ1 'newRect
+                                             (list (make-jump 'circ1)
+                                                   (make-stopAll)))))))
+                     
 
 
-;; interpreter: makes animationRunner: list of collisionEvents, list Objects, list repeat commands;
-;;    runs original command list once, then starts animation.
-;; Running; will check collisions (go through list of objects, check each for collide -> give back list of collides (2d (ab)(ac))
-;;          will then run all repeat commands 
 
